b86bbdf7b12d7c2599250dd52354aa8a14aaddbe
aydentownsley <aydentownsley@gmail.com>
2020-12-20 21:32:32 +0000

    adding pchar and pstr

--- char_op.c
+++ char_op.c
@@ -1,27 +1,53 @@
 #include "monty.h"
 
 /**
- * p_char -
+ * p_char - print char at top of stack/queue
  *
- * @
+ * @stack: head of stack/queue
  *
- * Return:
+ * Return: void, prints character
  */
 
 void p_char(stack_t **stack, unsigned int line_number __attribute__((unused)))
 {
+	stack_t *tmp;
 
+	if (stack && *stack)
+	{
+		tmp = *stack;
+		if (tmp->n > 0 && tmp->n <= 127)
+		{
+			putchar(tmp->n);
+			putchar('\n');
+		}
+		else
+			hand_exit(IN_PCHAR_EXIT, NULL);
+	}
+	hand_exit(PCHAR_EXIT, NULL);
 }
 
 /**
- * p_str -
+ * p_str - prints a string from the top of the stack
  *
- * @
+ * @stack: head of the stack/queue
  *
- * Return:
+ * Return: void, prints string
  */
 
 void p_str(stack_t **stack, unsigned int line_number __attribute__((unused)))
 {
+	stack_t *tmp = *stack;
 
+	if (stack && tmp)
+	{
+		while (tmp)
+		{
+			if (tmp->n > 0 && tmp->n <= 127)
+				putchar(tmp->n);
+			else if (tmp->n == 0)
+				break;
+			tmp = tmp->next;
+		}
+	}
+	putchar('\n');
 }
