346f096630b0440365c1e212635ac8a5f9c8e0eb
Ben Dosch <Benjamindosch@gmail.com>
2020-12-21 19:21:14 +0000

    opcode functions for stack queue and nop set up

---
+++ sqn_oc.c
@@ -0,0 +1,43 @@
+#include "monty.h"
+/* prototypes not added yet */
+/**
+ * nop -
+ *
+ * @stack: the pointer to the head of the stack;
+ * @line_number: current line number
+ *
+ * Return: void
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	return;
+}
+
+/**
+ * stack -
+ *
+ * @stack: the pointer to the head of the stack;
+ * @line_number: current line number
+ *
+ * Return: void
+ */
+
+void stack(stack_t **stack, unsigned int line_number)
+{
+	return;
+}
+
+/**
+ * queue -
+ *
+ * @stack: the pointer to the head of the stack;
+ * @line_number: current line number
+ *
+ * Return: void
+ */
+
+void queue(stack_t **stack, unsigned int line_number)
+{
+	return;
+}
