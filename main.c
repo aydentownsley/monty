472585019a91ed012844c6b04bbfd4645fd741d8
aydentownsley <aydentownsley@gmail.com>
2020-12-22 22:07:59 +0000

    fixing cant open file error

--- main.c
+++ main.c
@@ -28,8 +28,8 @@ int main(int argc, char *argv[])
 	fp = fopen(argv[1], "r");
 	if (fp == NULL)
 	{
-		status = OPEN_EXIT;
-		hand_exit(buffer, &stack, line_number, fp);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
 	}
 	while (getline(&buffer, &buff_size, fp) > 0)
 	{
